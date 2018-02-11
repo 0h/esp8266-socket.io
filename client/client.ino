d3342b65020aa426ef31e5d6d9d2cd9fb261fa9e
halil bayrakci <halilb@gmail.com>
2018-02-11 06:03:59 +0300

    initial commit

---
+++ client/client.ino
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <SocketIoClient.h>
+
+#define USE_SERIAL Serial
+
+ESP8266WiFiMulti WiFiMulti;
+SocketIoClient socket;
+
+void event(const char * payload, size_t length) {
+  USE_SERIAL.printf("got message: %s\n", payload);
+}
+
+void setup() {
+    USE_SERIAL.begin(115200);
+
+    USE_SERIAL.setDebugOutput(true);
+
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+
+      for(uint8_t t = 4; t > 0; t--) {
+          USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+          USE_SERIAL.flush();
+          delay(1000);
+      }
+
+    WiFiMulti.addAP("SSID", "PASSWORD");
+
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        delay(100);
+    }
+
+    socket.on("event", event);
+    socket.begin("localhost", 3000);
+}
+
+void loop() {
+    socket.loop();
+}
